927f2ce6ca4067d821297ff1d2a7bfdc5b772827
Alexander Erben <info@it-erben.com>
2022-05-14 14:43:17 +0200

    Implement play command

---
+++ src/message-handlers/thesmellshandler.ts
@@ -0,0 +1,47 @@
+import {MessageHandler} from "./messageHandler";
+import {Message, MessageAttachment, MessageEmbed} from "discord.js";
+import {extractMessageWithoutCommand} from "../utils";
+
+
+type Mapping = MappingEntry[]
+type MappingEntry = {
+    keys: string[],
+    url: string
+}
+
+const mapping: Mapping = [
+    {
+        "keys" : ["smells", "smell", "thesmells"],
+        "url" : "https://s3.eu-central-1.amazonaws.com/bots.funkymonks.xyz/the_SMELLS.mp3"
+    },
+    {
+        "keys" : ["cum", "cumslow", "cumslownow", "come", "comeslow", "comeslownow"],
+        "url" : "https://s3.eu-central-1.amazonaws.com/bots.funkymonks.xyz/cum_slow_now.mp3"
+    },
+    {
+        "keys" : ["makeyoucum", "makeyoucome"],
+        "url" : "https://s3.eu-central-1.amazonaws.com/bots.funkymonks.xyz/frusciante_fanfare.mp3"
+    }
+
+
+]
+
+export class TheSmellsHandler extends MessageHandler {
+
+    triggerMessageSubstrings(): string[] {
+        return ["play"]
+    }
+
+    async handle(message: Message): Promise<void> {
+        const messageArguments = extractMessageWithoutCommand(message);
+        const entry = mapping.find(entry => entry.keys.includes(messageArguments.toLowerCase()))
+        if (!entry) {
+            message.channel.send("File not known")
+        } else {
+            const messageAttachment = new MessageAttachment(entry.url);
+            message.channel.send({files:[messageAttachment]});
+        }
+
+    }
+
+}
