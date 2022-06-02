b4c7c7d1e3cbeea71ef7b895666019b5f9f6ab2b
Alexander Erben <info@it-erben.com>
2022-06-02 11:48:07 +0200

    Remove lyrics trivia game

--- src/message-handlers/lyricsTriviaMessageHandler.ts
+++ src/message-handlers/lyricsTriviaMessageHandler.ts
@@ -1,10 +1,5 @@
 import { MessageHandler } from "./messageHandler";
-import { HexColorString, Message, MessageEmbed } from "discord.js";
-import { getRandomSongSectionByArtist } from "./lyricsTriviaService";
-import { logger } from "../logger";
-import { extractMessageWithoutCommand } from "../utils";
-
-const WAIT_TIME_SECONDS = 15;
+import { Message } from "discord.js";
 
 export class LyricsTriviaMessageHandler extends MessageHandler {
   triggerMessageSubstrings(): string[] {
@@ -12,72 +7,8 @@ export class LyricsTriviaMessageHandler extends MessageHandler {
   }
 
   async handle(message: Message): Promise<void> {
-    logger.info(`Starting lyricsTrivia game with message ${message}.`);
-
-    message.channel.sendTyping();
-    const embedColor: HexColorString = `#${Math.floor(
-      Math.random() * 16777215
-    ).toString(16)}`;
-
-    const messageArguments = extractMessageWithoutCommand(message);
-
-    logger.info(`Message arguments: ${messageArguments}.`);
-
-    if (!messageArguments) return;
-
-    try {
-      const songObject = await getRandomSongSectionByArtist(
-        message,
-        messageArguments
-      );
-      if (!songObject) {
-        message.channel.send(
-          "An error happened 😬 Please try again, it might work."
-        );
-        return;
-      }
-
-      try {
-        const songEmbed = new MessageEmbed()
-          .setColor(embedColor)
-          .setTitle("Guess this song from " + songObject.artist)
-          .setDescription(songObject.section)
-          .setTimestamp()
-          .setThumbnail(
-            "https://ichef.bbci.co.uk/news/976/cpsprodpb/13F53/production/_83874718_thinkstockphotos-104548222.jpg"
-          )
-          .setFooter({
-            text: `💿 Guess in ${WAIT_TIME_SECONDS} seconds`,
-          });
-
-        const songSecondEmbed = new MessageEmbed()
-          .setColor(embedColor)
-          .setTitle(songObject.title)
-          .setTimestamp()
-          .setImage(songObject.art)
-          .setURL(songObject.url)
-          .setFooter({
-            text: "💿 - " + songObject.artist,
-          });
-        message.channel.send({ embeds: [songEmbed] });
-        setTimeout(() => {
-          message.channel.send({
-            embeds: [songSecondEmbed],
-          });
-        }, WAIT_TIME_SECONDS * 1000);
-      } catch (error) {
-        const songSecondEmbed = new MessageEmbed()
-          .setColor(embedColor)
-          .setDescription("An error happened 😬, try again!")
-          .setTimestamp();
-
-        message.channel.send({ embeds: [songSecondEmbed] });
-      }
-    } catch (error) {
-      logger.error(error);
-      message.channel.send(
-        "An error happened 😬 Please try again, it might work."
-      );
-    }
+    message.channel.send(
+      "This command is now a slash command. Please use /lyricsgame"
+    );
   }
 }
